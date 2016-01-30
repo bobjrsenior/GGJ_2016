2cc55c633be67acf4e5c37032cd7221719150e51
Camlin <camlin@carolina.rr.com>
2016-01-29 19:42:17 -0500

    Title, Instructions, and scene changes

---
+++ GGJ_2016/Assets/SceneSwitch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Starts Game
+    public void StartGame()
+    {
+        Application.LoadLevel("demo");
+    }
+
+    //Loads Instructions
+    public void Instructions()
+    {
+        Application.LoadLevel("Demo Inst");
+    }
+
+    //Goes back to Start
+    public void ReturnStart()
+    {
+        Application.LoadLevel("Demo Start");
+    }
+}
