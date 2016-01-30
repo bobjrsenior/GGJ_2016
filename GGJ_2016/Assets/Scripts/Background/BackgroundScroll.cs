3129408933df2e54566ed11d1ff9138c93dcb82d
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-30 14:17:12 -0500

    Added script to loop background. Demonstrated in Demo

---
+++ GGJ_2016/Assets/Scripts/Background/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public Transform player;
+
+    public Rigidbody2D playerRB;
+
+    public float minDistance;
+
+
+
+	// Update is called once per frame
+	void Update () {
+	    if(Mathf.Abs(player.position.x - transform.position.x) > minDistance)
+        {
+            if(playerRB.velocity.x > 0) {
+                transform.position = new Vector2(player.position.x - (player.position.x % minDistance) + minDistance, transform.position.y);
+            }
+            else
+            {
+                transform.position = new Vector2(player.position.x - (player.position.x % minDistance) - minDistance, transform.position.y);
+            }
+        }
+	}
+}
