5bcb4c73f79fb92d7c7fd9e531d5eb1ca8155b15
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-30 14:22:32 -0500

    Added comments to BackgroundScroll

--- GGJ_2016/Assets/Scripts/Background/BackgroundScroll.cs
+++ GGJ_2016/Assets/Scripts/Background/BackgroundScroll.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class BackgroundScroll : MonoBehaviour {
 
+    /// <summary>
+    /// The transform of the player to position near it
+    /// </summary>
     public Transform player;
 
+    /// <summary>
+    /// The players rigidbody in order to know which way the player is going
+    /// </summary>
     public Rigidbody2D playerRB;
 
+    /// <summary>
+    /// The minimum distance before swapping sides (Distance in order to keep a clean loop)
+    /// </summary>
     public float minDistance;
 
 
 
 	// Update is called once per frame
 	void Update () {
+        //Is the background too far away?
 	    if(Mathf.Abs(player.position.x - transform.position.x) > minDistance)
         {
+            //Swap sides to the side the player is going to the nearest minDistance (keeps clean loop)
             if(playerRB.velocity.x > 0) {
                 transform.position = new Vector2(player.position.x - (player.position.x % minDistance) + minDistance, transform.position.y);
             }
