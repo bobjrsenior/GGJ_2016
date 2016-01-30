20af6ed1499eaaf2d82fd34e92211b5d0d201cd2
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-30 02:47:38 -0500

    Added a boundary and ability to move player on main menu

---
+++ GGJ_2016/Assets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(0, 0, -10);
+	}
+}
