e0c7882e7220fd43aa031a871de08206732021d8
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-30 01:41:45 -0500

    Added timer ui to track how long a level is taking

---
+++ GGJ_2016/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private Text text;
+
+    private float time = 0.0f;
+	
+    public void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        text.text = "" + (int)(time * 100) / 100.0f;
+	}
+}
