28ad5874654a0db6050150298ac7698b7f4e4b7b
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-30 14:53:15 -0500

    Added max level for an item

--- GGJ_2016/Assets/Scripts/Levels/MinLevel.cs
+++ GGJ_2016/Assets/Scripts/Levels/MinLevel.cs
@@ -8,10 +8,15 @@ public class MinLevel : MonoBehaviour {
     /// </summary>
     public int minLevel = -1;
 
+    /// <summary>
+    /// The last level this object will spawn on
+    /// </summary>
+    public int maxLevel = 999;
+
     public void Awake()
     {
         //If the current level is too low, destroy this object
-        if(minLevel > LevelCounter.levelCounter.level)
+        if(minLevel > LevelCounter.levelCounter.level || maxLevel < LevelCounter.levelCounter.level)
         {
             Destroy(this.gameObject);
         }//If minLevel hasn't been set, add a debug message
