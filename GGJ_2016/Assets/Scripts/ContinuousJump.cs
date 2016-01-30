a2f9b6685fb66bb963107eedb3fa20e083248330
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-29 23:01:17 -0500

    Added an enemy who continuously jumps, prefabs for the long platform, and sliced out the buttons/doors

---
+++ GGJ_2016/Assets/Scripts/ContinuousJump.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinuousJump : MonoBehaviour {
+
+    /// <summary>
+    /// How fast jumping moves you up in Units/Second
+    /// </summary>
+    private float jumpSpeed = 7.5f;
+
+
+    /// <summary>
+    /// New keyword to hide warning about hiding rigidbody keyword
+    /// </summary>
+    private new Rigidbody2D rigidbody;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+
+    }
+}
